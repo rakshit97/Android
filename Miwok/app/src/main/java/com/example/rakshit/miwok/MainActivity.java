12dd713ba3b87ccb4f8093901ba66ff18142ef3a
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2016-12-11 15:18:28 +0530

    Added and linked activities

--- Miwok/app/src/main/java/com/example/rakshit/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/rakshit/miwok/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.rakshit.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchNumbersActivity(View view)
+    {
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    public void launchFamilyActivity(View view)
+    {
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+
+    public void launchColorsActivity(View view)
+    {
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+
+    public void launchPhrasesActivity(View view)
+    {
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
     }
 }
