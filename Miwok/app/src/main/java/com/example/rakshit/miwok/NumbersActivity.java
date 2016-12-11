adc9f67e079d563f29b28bce992290eea25a6326
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2016-12-11 20:21:21 +0530

    Added ListView and ArrayAdapter for Numbers Activity

--- Miwok/app/src/main/java/com/example/rakshit/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/rakshit/miwok/NumbersActivity.java
@@ -1,13 +1,38 @@
 package com.example.rakshit.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class NumbersActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList <String> eng = new ArrayList<String>(10);
+        eng.add("one");
+        eng.add("two");
+        eng.add("three");
+        eng.add("four");
+        eng.add("five");
+        eng.add("six");
+        eng.add("seven");
+        eng.add("eight");
+        eng.add("nine");
+        eng.add("ten");
+
+        //LinearLayout rootView = (LinearLayout)findViewById(R.id.activity_numbers);
+        //TextView[] nums = new TextView[10];
+        ArrayAdapter<String> numsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eng);
+
+        ListView list = (ListView)findViewById(R.id.nums_list);
+        list.setAdapter(numsAdapter);
     }
 }
