a0b9bcc330bff320fdd4e9b046e5b95d6c94e7d3
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2016-12-12 19:01:08 +0530

    Added audio files

--- MusicPlayer/app/src/main/java/com/example/rakshit/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/rakshit/musicplayer/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.rakshit.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    MediaPlayer song;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        song = MediaPlayer.create(this, R.raw.fightsong);
+    }
+
+    public void play_song(View view)
+    {
+        Toast.makeText(this, "Playing Fight Song by Rachel Platten",Toast.LENGTH_SHORT).show();
+        song.start();
+    }
+
+    public void pause_song(View view)
+    {
+        Toast.makeText(this, "Paused",Toast.LENGTH_SHORT).show();
+        song.pause();
     }
 }
