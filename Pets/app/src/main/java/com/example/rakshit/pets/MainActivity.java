f63f8d65247028083a79593847402e5bbd1c4d6e
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2016-12-25 15:59:30 +0530

    Added menu to MainActivity and created AddActivity

--- Pets/app/src/main/java/com/example/rakshit/pets/MainActivity.java
+++ Pets/app/src/main/java/com/example/rakshit/pets/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.rakshit.pets;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.mainactivity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.dummy: return true;
+            case R.id.del_all: return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
