9aad473262ffb3ada51834d254a764b1ce91f778
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2017-02-17 21:35:00 +0530

    Added UriMatcher

--- Pets/app/src/main/java/com/example/rakshit/pets/data/PetsProvider.java
+++ Pets/app/src/main/java/com/example/rakshit/pets/data/PetsProvider.java
@@ -2,6 +2,7 @@ package com.example.rakshit.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
@@ -9,6 +10,16 @@ import android.support.annotation.Nullable;
 public class PetsProvider extends ContentProvider
 {
     private PetsDBHelper dbHelper;
+    private static final int WHOLE_TABLE = 1000;
+    private static final int SPECIFIC_COLUMN = 1001;
+
+    private static final UriMatcher uri_matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static
+    {
+        uri_matcher.addURI(PetsContract.CONTENT_AUTHORITY, PetsContract.PETS_PATH, WHOLE_TABLE);
+        uri_matcher.addURI(PetsContract.CONTENT_AUTHORITY, PetsContract.PETS_PATH + "/#", SPECIFIC_COLUMN);
+    }
 
     @Override
     public boolean onCreate()
