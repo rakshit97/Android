93b7ba6488933dbbdf90da937f3b76ebe1bb633b
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2017-03-12 18:16:12 +0530

    Started project Sunshine(Weather App)

---
+++ Sunshine/app/src/main/java/com/example/rakshit/sunshine/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.rakshit.sunshine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.container_main, new MainActivityFragment()).commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if(id == R.id.menu_settings)
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MainActivityFragment extends Fragment
+    {
+        public MainActivityFragment()
+        {
+
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
