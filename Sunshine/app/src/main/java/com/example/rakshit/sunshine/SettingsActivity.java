0156fb4d82db67b75cd5dacdc8045ee797ffeeae
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2017-03-23 18:30:03 +0530

    Added SettingsActivity

---
+++ Sunshine/app/src/main/java/com/example/rakshit/sunshine/SettingsActivity.java
@@ -0,0 +1,40 @@
+package com.example.rakshit.sunshine;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void bindPreferenceSummaryToValue(Preference preference)
+    {
+        preference.setOnPreferenceChangeListener(this);
+        String preferencesString = PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), "");
+        onPreferenceChange(preference, preferencesString);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object value)
+    {
+        String valueString = value.toString();
+        if(preference instanceof ListPreference)
+        {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(valueString);
+            if(prefIndex >= 0)
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+        }
+        else
+            preference.setSummary(valueString);
+
+        return true;
+    }
+}
