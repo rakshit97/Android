0fc7bc931a38c9e0f420bb1f156c7a1d0ccb37ea
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2017-03-23 21:20:20 +0530

    Added Share option in Details Activity

--- Sunshine/app/src/main/java/com/example/rakshit/sunshine/DetailActivity.java
+++ Sunshine/app/src/main/java/com/example/rakshit/sunshine/DetailActivity.java
@@ -5,16 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity
 {
+    private ShareActionProvider shareActionProvider;
+    private static String dataString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,7 +33,19 @@ public class DetailActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+        MenuItem item = menu.findItem(R.id.menu_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if(shareActionProvider != null)
+            shareActionProvider.setShareIntent(createShareIntent());
         return true;
+    }
+
+    private Intent createShareIntent()
+    {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, dataString+"#Sunshine App");
+        return shareIntent;
     }
 
     @Override
@@ -61,7 +77,8 @@ public class DetailActivity extends AppCompatActivity
             TextView tv_details = (TextView)rootView.findViewById(R.id.tv_details);
 
             ForecastData data = getActivity().getIntent().getParcelableExtra("data");
-            tv_details.setText(String.valueOf(data.getTemp_max()) + "/" + String.valueOf(data.getTemp_min())+ " - " + data.getCondition());
+            dataString = String.valueOf(data.getTemp_max()) + "/" + String.valueOf(data.getTemp_min())+ " - " + data.getCondition();
+            tv_details.setText(dataString);
             return rootView;
         }
     }
