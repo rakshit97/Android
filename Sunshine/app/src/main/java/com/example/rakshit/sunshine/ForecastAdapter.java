348159d861ddeec7fcc099470c651d3619940a82
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2017-05-15 20:53:42 +0530

    Multiple list view items in a single list

--- Sunshine/app/src/main/java/com/example/rakshit/sunshine/ForecastAdapter.java
+++ Sunshine/app/src/main/java/com/example/rakshit/sunshine/ForecastAdapter.java
@@ -6,14 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.rakshit.sunshine.data.WeatherContract.WeatherEntries;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class ForecastAdapter extends CursorAdapter
 {
+    private static final int TYPE_TODAY = 0;
+    private static final int TYPE_OTHERS = 1;
     private static final String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
     public ForecastAdapter(Context context, Cursor cursor)
@@ -24,29 +29,59 @@ public class ForecastAdapter extends CursorAdapter
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup)
     {
-        return LayoutInflater.from(context).inflate(R.layout.list_forecast, viewGroup, false);
+        int type = getItemViewType(cursor.getPosition());
+        int layout = type==TYPE_TODAY?R.layout.list_forecast_today:R.layout.list_forecast;
+        View view =  LayoutInflater.from(context).inflate(layout, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        view.setTag(holder);
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor)
     {
-        TextView tv_day = (TextView) view.findViewById(R.id.tv_day);
-        TextView tv_desc = (TextView) view.findViewById(R.id.tv_desc);
-        TextView tv_max_temp = (TextView) view.findViewById(R.id.tv_max_temp);
-        TextView tv_min_temp = (TextView) view.findViewById(R.id.tv_min_temp);
-
+        ViewHolder holder = (ViewHolder) view.getTag();
         String day = getDay(cursor.getLong(cursor.getColumnIndexOrThrow(WeatherEntries.COLUMN_DATE)));
         String desc = cursor.getString(cursor.getColumnIndexOrThrow(WeatherEntries.COLUMN_SHORT_DESC));
         double x = cursor.getDouble(cursor.getColumnIndexOrThrow(WeatherEntries.COLUMN_MAX_TEMP));
         double n = cursor.getDouble(cursor.getColumnIndexOrThrow(WeatherEntries.COLUMN_MIN_TEMP));
 
-        String max = String.valueOf(Math.round(x));
-        String min = String.valueOf(Math.round(n));
+        double max = Math.round(x);
+        double min = Math.round(n);
 
-        tv_day.setText(day);
-        tv_desc.setText(desc);
-        tv_max_temp.setText(max);
-        tv_min_temp.setText(min);
+        holder.tv_day.setText(day);
+        holder.tv_desc.setText(desc);
+        holder.tv_max_temp.setText(context.getString(R.string.format_temp, max));
+        holder.tv_min_temp.setText(context.getString(R.string.format_temp, min));
+    }
+
+    private static class ViewHolder
+    {
+        TextView tv_day;
+        TextView tv_desc;
+        TextView tv_max_temp;
+        TextView tv_min_temp;
+        ImageView icon;
+        public ViewHolder(View view)
+        {
+            tv_day = (TextView) view.findViewById(R.id.tv_day);
+            tv_desc = (TextView) view.findViewById(R.id.tv_desc);
+            tv_max_temp = (TextView) view.findViewById(R.id.tv_max_temp);
+            tv_min_temp = (TextView) view.findViewById(R.id.tv_min_temp);
+            icon = (ImageView) view.findViewById(R.id.icon);
+        }
+    }
+
+    @Override
+    public int getViewTypeCount()
+    {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position)
+    {
+        return position==0?TYPE_TODAY:TYPE_OTHERS;
     }
 
     private String getDay(long time)
@@ -57,11 +92,13 @@ public class ForecastAdapter extends CursorAdapter
         int today = getToday();
 
         if (day==getToday())
-            return "Today";
+        {
+            String ret =  new SimpleDateFormat("MMM D").format(new Date(time * 1000L));
+            return "Today, "+ret;
+        }
         else if (day==((getToday()+1)%8)+1)
             return "Tomorrow";
         return days[day-1];
-        //return new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(time * 1000L));
     }
 
     private int getToday()
