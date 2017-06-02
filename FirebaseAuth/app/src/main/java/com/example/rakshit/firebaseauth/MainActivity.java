830de751227fc08dce13230e13f8a7cb8923eaea
Rakshit Thakkar <rakshitthakkar97@gmail.com>
2017-06-02 20:40:35 +0530

    Simple application using Firebase Auth for registering new user, login, manage session, check status and reset password using e-mail and password

---
+++ FirebaseAuth/app/src/main/java/com/example/rakshit/firebaseauth/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.rakshit.firebaseauth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
+        Button reg = (Button)findViewById(R.id.register);
+        Button login = (Button)findViewById(R.id.login);
+        Button check = (Button)findViewById(R.id.check);
+        TextView forgot = (TextView)findViewById(R.id.forgot);
+        final EditText email = (EditText)findViewById(R.id.email);
+        final EditText pass = (EditText)findViewById(R.id.pass);
+
+        reg.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                auth.createUserWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString().trim()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                            Toast.makeText(MainActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                auth.signInWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString().trim()).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                            Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        check.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(auth.getCurrentUser()!=null)
+                    Toast.makeText(MainActivity.this, "Currently logged in", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(MainActivity.this, "Currently logged out", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        forgot.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                auth.sendPasswordResetEmail(email.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                            Toast.makeText(MainActivity.this, "Reset mail sent", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+}
